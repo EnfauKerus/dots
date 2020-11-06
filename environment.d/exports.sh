f4c80d2fba0ac822f5f979a76c8bd69c121fbc13
Enfau Kerus <enfaukerus@disroot.org>
2020-11-06 09:56:24 +0800

    initial commit

---
+++ environment.d/exports.sh
@@ -0,0 +1,4 @@
+export EDITOR='vim'
+export SDL_AUDIODRIVER="alsa"
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export _JAVA_OPTIONS=-Dawt.useSystemAAFontSettings=on
---
+++ profile.d/aliases.sh
@@ -0,0 +1,5 @@
+alias cd..='cd ..'
+alias cls="echo -ne '\ec'"
+alias mv="mv -i"
+alias cp="cp -i"
+alias rm="rm -I"
